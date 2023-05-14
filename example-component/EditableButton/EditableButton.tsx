5d7bc2f863e083e8a633703221d21973720dfb15
Muhammad Faizan Jamshed <43693296+faizan-alam@users.noreply.github.com>
2023-05-14 23:07:10 +0500

    Update file

--- example-component/EditableButton/EditableButton.tsx
+++ example-component/EditableButton/EditableButton.tsx
@@ -1,23 +1,23 @@
 import * as React from "react";
 import moment from "moment";
 
-type Props = {
+type TestButtonProps = {
   test: string;
   test2: string;
   test3: () => void;
 };
 
-export const TestButton: React.FC<Props> = (props) => {
-  const { test, test2, ...otherProps } = props;
+export const TestButton: React.FC<TestButtonProps> = (props) => {
+  const { test, test2, test3, ...otherProps } = props;
   return (
-    <button test={test} test2={test2} test3={props.test3} {...otherProps}>
+    <button test={test} test2={test2} teste={props.test3} {...otherProps}>
       {title}
     </button>
   );
 };
 
 TestButton.defaultProps = {
-  test: "red",
-  test2: "black",
+  test: red,
+  test2: black,
   test3: () => null,
 };
